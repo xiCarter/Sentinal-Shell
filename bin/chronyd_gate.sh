37e16073ad45f58e87a687fd1db9c49f69585e7a
Dev-xCarter <xflowers11@gmail.com>
2026-05-19 14:03:04 -0400

    feat(monitoring) add healthcheck helper + makestep escalation to chronyd_gate.sh for large jitter disconnects

--- bin/chronyd_gate.sh
+++ bin/chronyd_gate.sh
@@ -6,28 +6,34 @@
 # CONFIG
 PROMPT_MESSAGE="TIME SYNC IS DISABLED - RESTARTING"
 
+# Helper Functions
+healthcheck () {
+	if timedatectl show --property=NTP | grep -q "NTP=yes" && \
+	systemctl is-active chronyd > /dev/null 2>&1; then
+	echo "=== CHRONYD GATE RESTORED ===" | tee /dev/console
+	exit 0
+else
+	echo "=== CHRONYD GATE FAILED TO RESTORE ===" | tee /dev/console
+	exit 2
+	fi
+}
+
 # HEALTH CHECK
 if timedatectl show --property=NTP | grep -q "NTP=yes" && \
 	systemctl is-active chronyd > /dev/null 2>&1; then
 	exit 0
 else
-	echo "=== CHRONYD GATE ALERT ==="
-	echo "$PROMPT_MESSAGE"
-	echo "Current Status"
+	echo "=== CHRONYD GATE ALERT ===" | tee /dev/console
+	echo "$PROMPT_MESSAGE" | tee /dev/console
+	echo "Current Status" | tee /dev/console
 	timedatectl
-	echo "=========================="
-	echo "RESTARTING"
+	echo "==========================" | tee /dev/console
+	echo "RESTARTING" | tee /dev/console
 	sudo systemctl daemon-reload
+	sudo chronyc makestep
 	sudo systemctl restart chronyd.service
 	sudo systemctl enable chronyd.service
 	sudo timedatectl set-ntp true
-	sleep 3
-		if timedatectl show --property=NTP | grep -q "NTP=yes" && \
-		systemctl is-active chronyd > /dev/null 2>&1; then
-			echo "=== CHRONYD GATE RESTORED ==="
-			exit 0
-		else
-			echo "=== CHRONYD GATE FAILED TO RESTART ==="
-			exit 2
-		fi
+	sleep 10
+	healthcheck 
 fi
